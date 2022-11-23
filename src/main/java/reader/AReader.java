e5a1f4cffbc1e9ab612d092d19a886abe3a20d23
squadrontechnology <75939808+khushalipatel1995@users.noreply.github.com>
2022-11-23 15:03:57 +0530

    Initial Code base

---
+++ src/main/java/reader/AReader.java
@@ -0,0 +1,108 @@
+package reader;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import alluxio.hadoop.FileSystem;
+import org.apache.parquet.column.page.PageReadStore;
+import org.apache.parquet.hadoop.ParquetFileReader;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class AlluxioFileReader implements Runnable {
+    FileSystem fs=null;
+    Path filePath;
+    public AlluxioFileReader(FileSystem fs , Path filePath){
+        this.fs=fs;
+        this.filePath=filePath;
+    }
+
+    @Override
+    public void run() {
+
+        FSDataInputStream in = null;
+        try {
+
+            ParquetFileReader reader = ParquetFileReader.open(this.fs.getConf(),this.filePath);
+            PageReadStore pages;
+            while ((pages = reader.readNextRowGroup()) != null) {
+                long rows = pages.getRowCount();
+                System.out.println(rows);
+            }
+            System.out.println("File Size: "+this.filePath.toString()+":");
+
+            /*
+            in = this.fs.open(this.filePath);
+            int numBytes=0;
+            long totalBytes=0;
+            byte[] b=new byte[1024*1024*64];
+            while (true) {
+                try {
+                    if (!((numBytes = in.read(b))> 0)) break;
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+                totalBytes=totalBytes+numBytes;
+            }
+            in.close();
+            System.out.println("File Size: "+this.filePath.toString()+":"+totalBytes);
+
+            */
+        } catch (IOException e) {
+           System.err.println(e);
+        }
+    }
+}
+
+public class AReader {
+    public  static  void main(String[] args) throws IOException {
+
+        if (args.length<1){
+            System.out.println("Error:");
+            System.out.println("USAGE: <JAVA CLASS NAME> <ALLUXIO_PATH> <THREADS>");
+        }
+
+
+        String pathStr=args[0];
+        int MAX_T=Integer.parseInt(args[1]);
+        System.out.println("Reading File : "+pathStr+" with N Threads:"+MAX_T);
+
+
+        //part-00000-e7d59cb6-125c-436e-a76a-f2d604e401ae-c000.snappy.parquet
+        FileSystem fs = new alluxio.hadoop.FileSystem();
+        Path path=new Path(pathStr);
+        Configuration conf=new Configuration();
+        fs.initialize(path.toUri(),conf);
+
+
+        ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
+
+        RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(
+                path, true);
+        long start = System.currentTimeMillis();
+        long totalFiles=0;
+        while(fileStatusListIterator.hasNext()){
+
+            LocatedFileStatus fileStatus = fileStatusListIterator.next();
+            //System.out.println(fileStatus.getPath());
+            if (!fileStatus.getPath().toString().contains("_SUCCESS")){
+
+
+            AlluxioFileReader reader=new AlluxioFileReader(fs,fileStatus.getPath());
+            pool.execute(reader);
+            totalFiles++;
+            }
+        }
+
+        System.out.println("File Submitted: "+totalFiles+" ");
+        pool.shutdown();
+        while(!pool.isTerminated()){}
+        long elapsedTime = System.currentTimeMillis() - start;
+        System.out.println("Total Time Taken: "+elapsedTime+" to read "+totalFiles+" files.");
+
+    }
+}
+
+
