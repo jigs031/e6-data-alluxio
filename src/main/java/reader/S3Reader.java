7c1a9c75e6262999a330ff76c9b07d5db67bfcb9
Bhaumik Soni <bhaumik0369@gmail.com>
2022-11-24 15:48:07 +0530

    s3 reader

--- src/main/java/reader/S3Reader.java
+++ src/main/java/reader/S3Reader.java
@@ -67,6 +67,7 @@ public class S3Reader {
         S3AFileSystem fs = new S3AFileSystem();
         Path path=new Path(pathStr);
         Configuration conf=new Configuration();
+        conf.set("fs.s3a.connection.maximum","100");
         fs.initialize(path.toUri(),conf);
 
 
